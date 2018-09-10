471f2ae3fb3adba2dadf880388a70f4bc1aebec6
Adam Humpherys <adam@adamh.us>
2018-09-10 16:54:30 -0600

    Initial source commit

---
+++ src/Cardknox.NET/CardknoxOperations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cardknox.NET
+{
+    public static class CardknoxOperations
+    {
+        public const string CCSale = "cc:sale";
+        public const string CCAuthOnly = "cc:authonly";
+        public const string CCSave = "cc:save";
+        public const string CCRefund = "cc:refund";
+    }
+}
---
+++ src/Cardknox.NET/CardknoxRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cardknox.NET
+{
+    public class CardknoxRequest
+    {
+        private string _key { get; }
+        private string _software { get; }
+        private string _softwareVersion { get; }
+        private string _cardknoxVersion { get; } = "4.5.5";
+        private const string Url = "https://x1.cardknox.com/gateway";
+
+        public CardknoxRequest(string key, string software, string softwareVersion, string cardknoxVer = null)
+        {
+            _key = key;
+            _software = software;
+            _softwareVersion = softwareVersion;
+            if (cardknoxVer != null)
+                _cardknoxVersion = cardknoxVer;
+        }
+
+
+    }
+}
